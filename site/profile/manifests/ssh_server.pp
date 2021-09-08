90834ec7122cf0e69911a4bcc91e57223fa5f91b
bozakovp <35504519+bozakovp@users.noreply.github.com>
2021-09-08 20:58:26 +0300

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCoh2Zggt6WqBFLyMwZU/9VH7cBYXW9D1DOsxmX6sDcCoTlj02NI+JC1PdDG/BGr0dV3ND+wkjGXABALeM6VZ7B6zJwqSYJJGHWfQiOG+xqy4nIafSMHWVx8uGdw/8FdDgiu2QnmQyM3Ctth6hjTKEsGa/1ghVzx6X3hEcb0xmlAvQaxCgNNFhq0XqLrjd7ingdAoP+3df1IwRou8d85ZsS2fGv7skSdwjg5Xu9JlVriRL7sgujLbR0cBsWP+hMeXHRkA1FG+CuXpleFuR2xtSc3UA0jf3GigLqgeZq6yfGhazDA9PU9Faitn6yo8ldKtXFfFixRPFAY+EK9ifJAiiJ',
   }  
 }
