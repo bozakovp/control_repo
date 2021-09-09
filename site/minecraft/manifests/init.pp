5430d29fcd8ac9de882998cd7cca9fb03a5dcd8f
bozakovp <35504519+bozakovp@users.noreply.github.com>
2021-09-09 09:56:05 +0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -6,9 +6,9 @@ class minecraft {
     ensure => file,
     source      => 'https://launcher.mojang.com/v1/objects/a16d67e5807f57fc4e550299cf20226194497dc2/server.jar',
   }
-  package {'java-1.7.0-openjdk.x86_64':
-    ensure => present,
-  }
+#  package {'java-1.7.0-openjdk.x86_64':
+#    ensure => present,
+#  }
   file {'/opt/minecraft/eula.txt':
     ensure => file,
     content => 'eula=true'
