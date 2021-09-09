87dc889e7ccef1353757085d48543661ed32c871
bozakovp <35504519+bozakovp@users.noreply.github.com>
2021-09-09 11:31:10 +0300

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -5,12 +5,12 @@ class minecraft (
   file {'$instasll_dir':
     ensure => directory,
 
-  file {"${install_dir}/minecraft_server.jar":
+  file { "${install_dir}/minecraft_server.jar":
     ensure => file,
     source => '$url,
     before => Service ['minecraft'],
   }
-  file {"${install_dir}/eula.txt":
+  file { "${install_dir}/eula.txt":
     ensure => file,
     content => 'eula=true'
   }
